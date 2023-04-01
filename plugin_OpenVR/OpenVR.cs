c9033f5db635943af6ee12c277e074e1a31f393d
公彦赤屋先 <kimihikoakaya@gmail.com>
2023-04-02 02:12:49 +0900

    Prepare the metadata for the plugin store

--- plugin_OpenVR/OpenVR.cs
+++ plugin_OpenVR/OpenVR.cs
@@ -38,6 +38,7 @@ namespace plugin_OpenVR;
 [ExportMetadata("Name", "SteamVR")]
 [ExportMetadata("Guid", "K2VRTEAM-AME2-APII-SNDP-SENDPTOPENVR")]
 [ExportMetadata("Publisher", "K2VR Team")]
+[ExportMetadata("Version", "1.0.0.0")]
 [ExportMetadata("Website", "https://github.com/KinectToVR/plugin_OpenVR")]
 public class SteamVR : IServiceEndpoint
 {
