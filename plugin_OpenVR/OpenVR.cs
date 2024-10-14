e330926a4bd9e2a21cb8c408221c59d0d0abb00c
公彦赤屋先 <kimihikoakaya@gmail.com>
2024-10-14 21:27:54 +0900

    Official Standable support

--- plugin_OpenVR/OpenVR.cs
+++ plugin_OpenVR/OpenVR.cs
@@ -1388,7 +1388,7 @@ public static class OvrExtensions
         return new dTrackerBase
         {
             ConnectionState = Convert.ToSByte(tracker.ConnectionState),
-            TrackingState = Convert.ToSByte(tracker.TrackingState != TrackedJointState.StateNotTracked),
+            TrackingState = Convert.ToSByte(tracker.TrackingState == TrackedJointState.StateTracked),
             Serial = tracker.Serial,
             Role = (dTrackerType)tracker.Role,
             Position = tracker.Position.ComVector(),
