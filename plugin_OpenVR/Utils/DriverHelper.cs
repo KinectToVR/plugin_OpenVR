c6c318ab62e7587640f4dd12e563cd9b93cb22fb
公彦赤屋先 <kimihikoakaya@gmail.com>
2024-12-22 06:10:36 +0900

    Fixed method names

--- plugin_OpenVR/Utils/DriverHelper.cs
+++ plugin_OpenVR/Utils/DriverHelper.cs
@@ -7,28 +7,34 @@ public class DriverHelper
 {
     public static int InstallDriverProxyStub(bool emulated)
     {
-        return emulated ? Install00ProxyStub() : InstallProxyStub();
+        return emulated ? Methods00.InstallProxyStub() : Methods.InstallProxyStub();
     }
 
     public static int UninstallDriverProxyStub(bool emulated)
     {
-        return emulated ? Uninstall00ProxyStub() : UninstallProxyStub();
+        return emulated ? Methods00.UninstallProxyStub() : Methods.UninstallProxyStub();
     }
-
-    [DllImport("driver_Amethyst.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern int InstallProxyStub();
-
-    [DllImport("driver_Amethyst.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern int UninstallProxyStub();
-
-    [DllImport("driver_00Amethyst.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern int Install00ProxyStub();
-
-    [DllImport("driver_00Amethyst.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern int Uninstall00ProxyStub();
 
     [DllImport("oleaut32.dll", PreserveSig = false)]
     public static extern void GetActiveObject(ref Guid rclsid, IntPtr pvReserved,
         [MarshalAs(UnmanagedType.IUnknown)] out object ppunk
     );
+
+    internal static class Methods
+    {
+        [DllImport("driver_Amethyst.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int InstallProxyStub();
+
+        [DllImport("driver_Amethyst.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int UninstallProxyStub();
+    }
+
+    internal static class Methods00
+    {
+        [DllImport("driver_00Amethyst.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int InstallProxyStub();
+
+        [DllImport("driver_00Amethyst.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int UninstallProxyStub();
+    }
 }
